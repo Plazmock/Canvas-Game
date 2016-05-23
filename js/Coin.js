41296a7e4050506c8e770c8288dc59f02f83d207
karonka <karonka@abv.bg>
2016-05-23 19:41:46 +0300

    image loader + improved constructors

--- js/Coin.js
+++ js/Coin.js
@@ -1,6 +1,6 @@
 class Coin extends CollisionBox{
-	constructor(x,y,imgw,imgh,h,w,type,state,hp){
-		super(x,y,imgw,imgh,h,w,type,state,hp);
+	constructor(x,y,imgw,imgh,h,w,hp){
+		super(x,y,imgw,imgh,h,w,'coin','spin',state,hp);
 	}
 }
 Images['coin'] = new Array();
--- js/Slime.js
+++ js/Slime.js
@@ -1,6 +1,6 @@
 class Slime extends Enemy{
-	constructor(x,y,imgw,imgh,h,w,type,state,hp, accelerationX, accelerationY, maxSpeedX, maxSpeedY){
-		super(x,y,imgw,imgh,h,w,type,state,hp, accelerationX, accelerationY, maxSpeedX, maxSpeedY);
+	constructor(x,y,imgw,imgh,h,w,hp, accelerationX, accelerationY, maxSpeedX, maxSpeedY){
+		super(x,y,imgw,imgh,h,w,'slime','walk',hp, accelerationX, accelerationY, maxSpeedX, maxSpeedY);
 	}
 	collideWithTerrain(x){};
 	//Enemy need static sound that will be created in init();
